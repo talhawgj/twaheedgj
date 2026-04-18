b2d4eaad1e9d6fea741b81d77dfeab4dc4191a2c
Talha Waheed <talhawaheed7807@gmail.com>
2026-04-19 00:25:59 +0500

    feat: add Microsoft Clarity Analytics script for enhanced user insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -220,6 +220,18 @@ export default function RootLayout({
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(websiteSchema) }}
         />
+        {/* Microsoft Clarity Analytics */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i+"?ref=bwt";
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "wdrl2owln2");
+            `
+          }}
+        />
       </head>
       <body className="min-h-full antialiased">
         {children}
